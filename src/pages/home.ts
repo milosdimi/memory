8a64d5fd9a60a4dd9edc209251c00ba93945fd85
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-03-25 18:00:52 +0100

    home editing, btn hover

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -6,7 +6,7 @@ export function renderHome(): string {
         <p class="home__subtitle">It's play time.</p>
         <h1 class="home__title">Ready to play?</h1>
         <button class="home__btn" id="btn-start">
-          <img src="/assets/icons/stadia_controller.png" alt="" class="home__btn-icon">
+          <img src="/assets/icons/stadia_controller_bnt.png" alt="" class="home__btn-icon">
           Play
           <span class="home__btn-arrow">&#8594;</span>
         </button>
